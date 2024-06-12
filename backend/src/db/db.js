174654e4246a413876b869b36f99daf8f82a159e
ved-thakre <vedantthakre7@gmail.com>
2024-06-12 23:53:29 +0530

    initialized frontend and backend

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import { DB_NAME } from '../constants.js';
+dotenv.config();
 
-// export const connectDB = async() => {
-//     const connectionRes = await 
-// }
+export const connectDB = async() => {
+    try {
+        const connectionRes = await mongoose.connect(
+          `${process.env.MONGO_URI}/${DB_NAME}`
+        );
+        console.log(`MongoDB Connected : ${connectionRes.connection.host}`.bgMagenta.bold);
+    } catch (error) {
+        console.log("MongoDB Connection Error : ".bgRed.bold, error);
+        process.exit(1);
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import { app } from './app.js';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { connectDB } from './db/db.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5050;
 
+connectDB();
 app.listen(PORT, () => {
-    console.log(`Server is Live on PORT ${PORT}`.bgMagenta.bold);
+    console.log(`Server is Live on PORT ${PORT}`.yellow.bold);
 })

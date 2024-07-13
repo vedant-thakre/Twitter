3371f179c03d2ffdebd410e80dbb16967e0424ac
ved-thakre <vedantthakre7@gmail.com>
2024-07-13 23:20:35 +0530

    changes

--- backend/src/controllers/listController.js
+++ backend/src/controllers/listController.js
@@ -127,9 +127,3 @@ export const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie("refreshToken", options)
     .json(new Response(200, {}, "User Logged Out"));
 });
-
-// Reporting 12/07/2024
-// Worked on Bug Fixed On Demand Education Page
-// Worked on Owner Create Education Quiz APIs
-// Worked on Owner Education List apis
-// Worked on Owner Education Detail APIs
